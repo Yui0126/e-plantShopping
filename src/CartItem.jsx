e48821bf17e2fb10fed7806c8ceacfaa4e887e23
Yui0126 <yui.naruse.94@gmail.com>
2025-03-03 19:21:35 +0900

    fix bug

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -21,6 +21,10 @@ const CartItem = ({ onContinueShopping }) => {
     onContinueShopping(e);
   };
 
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
+
   const handleIncrement = (item) => {
     dispatch(updateQuantity({name: item.name, quantity: item.quantity+1}));
   };
@@ -39,7 +43,10 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
-    return item.quantity*parseInt(item.cost.slice(1))*item.quantity;   
+    let totalCost = 0;
+    const itemCost = parseItemCostToInteger(item.cost);
+    totalCost = item.quantity * itemCost;
+    return totalCost; 
   };
 
   return (
@@ -67,7 +74,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
